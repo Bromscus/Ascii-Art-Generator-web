e7604dbcca0d5a66afe12a0b0a8e971a51dca5d1
Bromscus <bromscus19772009@gmail.com>
2026-09-09 00:17:49 +0300

    added messages for each error

--- functions/status.go
+++ functions/status.go
@@ -20,36 +20,31 @@ type PageData struct {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// if the path is neither a / or ascii-art we show this message
 	if r.URL.Path != "/" && r.URL.Path != "/ascii-art" {
-		ErrorPage(w, "404: page not found", http.StatusNotFound)
+		ErrorPage(w, "404: page not found", "Please check that the Website address is spelled correctly or go to our home page.", http.StatusNotFound)
 
 		return
 	}
 	// template allows Go to generate a HTML output
 	if !ascii.CheckFile("templates/index.html", htmlHash) {
-		ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+		ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 		return
 	}
 	templ, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		if os.IsNotExist(err) {
-			ErrorPage(w, "404: Template Not Found", http.StatusNotFound)
-			return
-		}
-
-		ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+		ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 		return
 	}
 	// Display the empty form when the home page is first opened.
 	if r.URL.Path == "/" && r.Method == http.MethodGet {
 		err = templ.Execute(w, PageData{})
 		if err != nil {
-			ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+			ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 		}
 		return
 	}
 	// If the user sends a non post request to /ascii-art
 	if r.URL.Path == "/ascii-art" && r.Method != http.MethodPost {
-		ErrorPage(w, "405: Method Not Allowed", http.StatusMethodNotAllowed)
+		ErrorPage(w, "405: Method Not Allowed", "This request method is not allowed for this page.", http.StatusMethodNotAllowed)
 		return
 	}
 	// text is the input in the text area
@@ -79,34 +74,34 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	switch font {
 	case "standard":
 		if !ascii.CheckFile("formats/standard.txt", StandardHash) {
-			ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+			ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 			return
 		}
 		fontFile = "formats/standard.txt"
 
 	case "shadow":
 		if !ascii.CheckFile("formats/shadow.txt", ShadowHash) {
-			ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+			ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 			return
 		}
 		fontFile = "formats/shadow.txt"
 
 	case "thinkertoy":
 		if !ascii.CheckFile("formats/thinkertoy.txt", ThinkertoyHash) {
-			ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+			ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 			return
 		}
 		fontFile = "formats/thinkertoy.txt"
 
 	default:
 
-		ErrorPage(w, "400: Bad Request", http.StatusBadRequest)
+		ErrorPage(w, "400: Bad Request", "The request could not be processed. Please check your input and try again.", http.StatusBadRequest)
 		return
 	}
 
 	banner, err := os.ReadFile(fontFile)
 	if err != nil {
-		ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+		ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 		return
 	}
 
@@ -117,7 +112,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	err = templ.Execute(w, data)
 	if err != nil {
-		ErrorPage(w, "500: Internal Server Error", http.StatusInternalServerError)
+		ErrorPage(w, "500: Internal Server Error", "Something went wrong on the server.", http.StatusInternalServerError)
 		return
 	}
 }
