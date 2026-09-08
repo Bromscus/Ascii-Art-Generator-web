4dea07164eb572f76cd220deb0b4f292aa78303a
Bromscus <bromscus19772009@gmail.com>
2026-09-08 18:44:05 +0300

    done

--- functions/status.go
+++ functions/status.go
@@ -13,16 +13,21 @@ type PageData struct {
 	Result string
 	Font   string
 }
+
 //
 //templ.Execute basically means we write the final HTML into the HTTP response
 //
 func Handler(w http.ResponseWriter, r *http.Request) {
-	//if the path is neither a / or ascii-art we show this message 
+	// if the path is neither a / or ascii-art we show this message
 	if r.URL.Path != "/" && r.URL.Path != "/ascii-art" {
 		http.Error(w, "404: Status page not found Error", http.StatusNotFound)
 		return
 	}
-//template allows Go to generate a HTML output
+	// template allows Go to generate a HTML output
+	if !ascii.CheckFile("templates/index.html", htmlHash) {
+		http.Error(w, "500: Status Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 	templ, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -33,7 +38,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "500: Status Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	//Display the empty form when the home page is first opened.
+	// Display the empty form when the home page is first opened.
 	if r.URL.Path == "/" && r.Method == http.MethodGet {
 		err = templ.Execute(w, PageData{})
 		if err != nil {
@@ -41,13 +46,13 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	//If the user sends a non post request to /ascii-art
+	// If the user sends a non post request to /ascii-art
 	if r.URL.Path == "/ascii-art" && r.Method != http.MethodPost {
 		http.Error(w, "405: Status Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	//text is the input in the text area
-	//font is the banner
+	// text is the input in the text area
+	// font is the banner
 	text := r.FormValue("text")
 	font := r.FormValue("styles")
 
@@ -57,10 +62,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			Font:   font,
 			Result: msg,
 		}
-		//the server sends the https response header that has status code (can be viewed in the network section in the inspect of the browser)
-		//a http response header is a metadata, in simple terms like a package of data
+		// the server sends the https response header that has status code (can be viewed in the network section in the inspect of the browser)
+		// a http response header is a metadata, in simple terms like a package of data
 		w.WriteHeader(http.StatusBadRequest)
-		//just to show the issue along with the status error code
+		// just to show the issue along with the status error code
 		err = templ.Execute(w, data)
 		if err != nil {
 			fmt.Println(err)
