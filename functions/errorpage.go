5244942c6af32cbf938b80812acc08dc563088b0
Bromscus <bromscus19772009@gmail.com>
2026-09-09 00:17:35 +0300

    added a struct so we can send a different message for each error

--- functions/errorpage.go
+++ functions/errorpage.go
@@ -6,18 +6,24 @@ import (
 	"net/http"
 )
 
+type ErrorData struct {
+	Error   string
+	Message string
+}
 
-
-func ErrorPage(w http.ResponseWriter, msg string, status int) {
+func ErrorPage(w http.ResponseWriter, errorText string, msg string, status int) {
 	templ, err := template.ParseFiles("templates/errors.html")
 	if err != nil {
 		http.Error(w, msg, status)
 		return
 	}
-	
-	w.WriteHeader(status)
 
-	err = templ.Execute(w, msg)
+	w.WriteHeader(status)
+	data := ErrorData{
+		Error:   errorText,
+		Message: msg,
+	}
+	err = templ.Execute(w, data)
 	if err != nil {
 		fmt.Println(err)
 	}
